bf37c83f93c02dc9fa8c73c67b019d2e79dcf96d
Flávio Menezes dos Reis <flavio-reis@pge.rs.gov.br>
2022-05-03 17:30:42 -0300

    react learn

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,43 +1,19 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className='bg-violet-500 px-4 h-10 rounded hover:bg-violet-700 transition-colors'>{props.text ?? 'Undefined'}</button>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="flex gap-2">
+    <Button text="Enviar"/>
+    <Button text="Cancelar"/>
+    <Button />
     </div>
   )
 }
