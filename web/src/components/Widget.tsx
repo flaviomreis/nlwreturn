eab0cfb94fda31d20128914f262f5772874da58d
Flavio Menezes dos Reis <flaviomreis@gmail.com>
2022-05-03 20:18:31 -0300

    Widget popover using headlessui

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,25 +1,16 @@
 import { Chat, ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
+import { Popover } from '@headlessui/react';
 
 export function Widget() {
-  const [isWidgetOpened, setWidgetOpened] = useState(false);
-
-  function toogleWidgetStatus() {
-    setWidgetOpened(!isWidgetOpened);
-  }
-
   return (
-    <div className="absolute bottom-5 right-5">
-      {isWidgetOpened && <p>Hello, world</p>}
-      <button
-        onClick={toogleWidgetStatus}
-        className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group"
-      >
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Hello, world</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
