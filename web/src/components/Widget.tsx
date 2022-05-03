9633546259d64eec6177ab08c4a571c1e9962cae
Flavio Menezes dos Reis <flaviomreis@gmail.com>
2022-05-03 20:14:22 -0300

    widget popover manual implementation

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,15 +1,25 @@
 import { Chat, ChatTeardropDots } from 'phosphor-react';
+import { useState } from 'react';
 
 export function Widget() {
+  const [isWidgetOpened, setWidgetOpened] = useState(false);
+
+  function toogleWidgetStatus() {
+    setWidgetOpened(!isWidgetOpened);
+  }
+
   return (
     <div className="absolute bottom-5 right-5">
-      <p>Hello, world</p>
-      <button className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group" >
-        <ChatTeardropDots className="w-6 h-6"/>
+      {isWidgetOpened && <p>Hello, world</p>}
+      <button
+        onClick={toogleWidgetStatus}
+        className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group"
+      >
+        <ChatTeardropDots className="w-6 h-6" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           Feedback
         </span>
       </button>
     </div>
-  )
+  );
 }
