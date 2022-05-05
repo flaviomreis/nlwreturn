e1d53b9987b4d5200918f775dadc888d72388693
Flávio Menezes dos Reis <flavio-reis@pge.rs.gov.br>
2022-05-05 17:54:01 -0300

    aula 3/5 53m

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,38 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  response.send({ user1: 'flavio' });
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'aaa',
+    pass: 'bbb',
+  },
+});
+
+app.post('/feedbacks', async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Flávio <flavio@f18s.f9s>',
+    subject: 'Novo feedback',
+    html: ['fsadfasd'],
+  });
+
+  return response.status(201).send(feedback);
 });
 
 app.listen(3333, () => {
