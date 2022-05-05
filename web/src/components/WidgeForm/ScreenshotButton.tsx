01fb4058787ee782c8908e3d9df534ccc72c3c32
Flavio Menezes dos Reis <flaviomreis@gmail.com>
2022-05-05 00:21:10 -0300

    aula 2/5

--- web/src/components/WidgeForm/ScreenshotButton.tsx
+++ web/src/components/WidgeForm/ScreenshotButton.tsx
@@ -1,10 +1,42 @@
-import { Camera } from 'phosphor-react';
+import { Camera, Trash } from 'phosphor-react';
 import html2canvas from 'html2canvas';
+import { useState } from 'react';
+import { Loading } from './Loading';
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+  screenshot: string | null;
+  onScreenshotTook: (screenshot: string | null) => void;
+}
+
+export function ScreenshotButton({
+  screenshot,
+  onScreenshotTook,
+}: ScreenshotButtonProps) {
+  const [isTakingSnapshot, setTakingSnapshot] = useState(false);
+
   async function takeScreenshotHandle() {
+    setTakingSnapshot(true);
     const canvas = await html2canvas(document.querySelector('html')!);
     const base64Image = canvas.toDataURL('image/png');
+    onScreenshotTook(base64Image);
+    setTakingSnapshot(false);
+  }
+
+  if (screenshot) {
+    return (
+      <button
+        type="button"
+        onClick={() => onScreenshotTook(null)}
+        className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transitions-colors"
+        style={{
+          backgroundImage: `url(${screenshot})`,
+          backgroundPosition: 'right bottom',
+          backgroundSize: 180,
+        }}
+      >
+        <Trash weight="fill" />
+      </button>
+    );
   }
 
   return (
@@ -13,7 +45,7 @@ export function ScreenshotButton() {
       onClick={takeScreenshotHandle}
       className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 "
     >
-      <Camera className="w-6 h-6" />
+      {isTakingSnapshot ? <Loading /> : <Camera className="w-6 h-6" />}
     </button>
   );
 }
